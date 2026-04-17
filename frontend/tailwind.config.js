8b376805c5ff396e0ac6f7e346c92dc4a683b283
Laksh Solanki <218278156+laksh-solanki@users.noreply.github.com>
2026-04-17 19:43:09 +0530

    feat: initialize global CSS design system and Tailwind configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
+  ],
+  theme:{
+    extend:{},
+  }
   plugins: [],
 };
